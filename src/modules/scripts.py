66f0d161558cb5c0ea5348e3724fe7913ae6b0ce
Andrey Stepanyuk <stepanyuk.aa@yandex.kz>
2022-05-27 23:14:28 +0600

    fix and run scripts

--- src/modules/scripts.py
+++ src/modules/scripts.py
@@ -10,13 +10,17 @@ class scripts():
         self.get_scripts()
         self.create_script()
         self.delite_script()
-        self.get_script_data()
+        self.run_script()
 
 
     def get_scripts(self):
         @self.backend.app.route('/get/scripts', methods=['GET'])
         def get_scripts():
-            return self._get_files()
+            files = self._get_files()
+            data = {}
+            for file in files:
+                data[file] = self._get_data(file)
+            return json.dumps(data)
 
     def create_script(self):
         @self.backend.app.route('/create/script', methods=['POST'])
@@ -42,18 +46,29 @@ class scripts():
 
             return "OK"
 
-    def get_script_data(self):
-        @self.backend.app.route('/get/script/data', methods=['POST'])
-        def get_script_data():
+    def run_script(self):
+        @self.backend.app.route('/run/script', methods=['POST'])
+        def run_script():
             if request.method == 'POST':
                 data = request.get_json()
-                title = data['title']
-
-                text = self._get_data(title)
-                return json.dumps(text)
+                output = self._run_script(data['title'])
+                return json.dumps(output)
+                
             else: print("is not POST")
+            return "False"
 
-            return "OK"
+    # def get_script_data(self):
+    #     @self.backend.app.route('/get/script/data', methods=['POST'])
+    #     def get_script_data():
+    #         if request.method == 'POST':
+    #             data = request.get_json()
+    #             title = data['title']
+
+    #             text = self._get_data(title)
+    #             return json.dumps(text)
+    #         else: print("is not POST")
+
+    #         return "OK"
 
 
     ########################################################################### 
@@ -63,18 +78,21 @@ class scripts():
 
     def _get_files(self):
         files = os.listdir(self.config.path)
-        return json.dumps(files)
+        return files
 
-    def _create_file(self, title, text):
-        file = open(f"{self.config.path}/{title}.py", "w")
+    def _create_file(self, file, text):
+        file = open(f"{self.config.path}/{file}.py", "w")
         file.writelines(text)
         file.close()
 
-    def _get_data(self, title):
+    def _get_data(self, file):
         path = self.config
-        file = open(f"{self.config.path}/{title}.py", 'r')
+        file = open(f"{self.config.path}/{file}", 'r')
         data = file.readlines()
         file.close()
-
+        data = ''.join(data)
         return data
-    
+
+    def _run_script(self, file):
+        output = os.popen(f"python3 {self.config.path}/{file}").read()
+        return output
